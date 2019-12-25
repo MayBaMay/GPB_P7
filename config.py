8cb48f51d37a30be9cbfff59917d7ee9190fb8a8
MayMay <maylis.baschet@gmail.com>
2019-12-25 19:43:21 +0100

    config os.environ.geget

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ from boto.s3.connection import S3Connection
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
 GOOGLE_JS_KEY = "AIzaSyDcpvFtRDSx4pt3Fuf8etylEYKyUfm9knM"
-GOOGLE_GEO_KEY = os.environ.get('GOOGLE_GEO_K')
+GOOGLE_GEO_KEY = os.environ.get('GOOGLE_GEO_KEY')
