4c10fe38874e98602ed4fe24cd92f4529127b201
MayMay <maylis.baschet@gmail.com>
2020-01-06 15:06:47 +0100

    test avec jskey in .env

--- config.py
+++ config.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import os
+from dotenv import load_dotenv
 
 
 #### get GOOGLE_GEO_KEY depending if running in localhost or heroku
 try:
-    from dotenv import load_dotenv
     load_dotenv()
-    GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
     DEBUG = True
 except:
-    GOOGLE_GEO_KEY = os.environ.get("GOOGLE_GEO_K")
     DEBUG = False
 
+GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
+GOOGLE_JS_KEY = os.getenv("GOOGLE_JS_K")
 
 
 # GOOGLE_GEO_KEY = os.popen('heroku config:get GOOGLE_GEO_K').readlines()
@@ -44,4 +44,3 @@ except:
 #     GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
-GOOGLE_JS_KEY = "AIzaSyDcpvFtRDSx4pt3Fuf8etylEYKyUfm9knM" # this key has a Website restrictions
