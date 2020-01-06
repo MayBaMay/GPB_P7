d421135814ac1a2c82d55d6a3facdfaff43b8b46
MayMay <maylis.baschet@gmail.com>
2020-01-06 10:16:15 +0100

    remove API_geo_key from config

--- config.py
+++ config.py
@@ -4,5 +4,5 @@
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
-GOOGLE_JS_KEY = "AIzaSyDcpvFtRDSx4pt3Fuf8etylEYKyUfm9knM"
-GOOGLE_GEO_KEY = "AIzaSyD5Z9lKhzF6QCYeB5-tTm4AcIl4FCQBy8E"
+GOOGLE_JS_KEY = "AIzaSyDcpvFtRDSx4pt3Fuf8etylEYKyUfm9knM" # this key has a Website restrictions
+GOOGLE_GEO_KEY = "YOUR_API_KEY"
