a483b04327e3ab8615e9dc0e51c0c42cb0723880
MayMay <maylis.baschet@gmail.com>
2020-01-06 11:57:03 +0100

    test var .env & var heroku

--- config.py
+++ config.py
@@ -2,23 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import os
+from dotenv import load_dotenv
 
 #### get GOOGLE_GEO_KEY depending if running in localhost or heroku
-# try:
+try:
+    load_dotenv()
+    GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
+except:
+    GOOGLE_GEO_KEY = os.environ.get('GOOGLE_GEO_K')
+
+# if running with heroku, 'IS_HEROKU' would be True
+# is_prod = os.environ.get('IS_HEROKU', None)
+
+# if is_prod:
+#     GOOGLE_GEO_KEY = os.environ.get('GOOGLE_GEO_K')
+# else:
 #     from dotenv import load_dotenv
 #     load_dotenv()
-#     GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_KEY")
-# except ImportError:
-#     GOOGLE_GEO_KEY = os.environ.get('GOOGLE_GEO_KEY')
-
-is_prod = os.environ.get('IS_HEROKU', None)
-
-if is_prod:
-    GOOGLE_GEO_KEY = os.environ.get('GOOGLE_GEO_KEY')
-else:
-    from dotenv import load_dotenv
-    load_dotenv()
-    GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_KEY")
+#     GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
