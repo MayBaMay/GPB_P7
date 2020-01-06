0f3ca36c005df4af7ac4c8e8189155072699b4a5
MayMay <maylis.baschet@gmail.com>
2020-01-06 14:13:37 +0100

    test getenv

--- config.py
+++ config.py
@@ -2,18 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import os
-# from dotenv import load_dotenv
-
-GOOGLE_GEO_K = "AIzaSyD5Z9lKhzF6QCYeB5-tTm4AcIl4FCQBy8E"
+from dotenv import load_dotenv
 
 #### get GOOGLE_GEO_KEY depending if running in localhost or heroku
-# try:
-#     load_dotenv()
-#     GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
-#     DEBUG = True
-# except:
-#     GOOGLE_GEO_KEY = GOOGLE_GEO_K
-#     DEBUG = False
+try:
+    load_dotenv()
+    GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
+    DEBUG = True
+except:
+    GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
+    DEBUG = False
 
 # GOOGLE_GEO_KEY = os.popen('heroku config:get GOOGLE_GEO_K').readlines()
 # GOOGLE_GEO_KEY = GOOGLE_GEO_KEY[0][1:40]
@@ -41,8 +39,6 @@ GOOGLE_GEO_K = "AIzaSyD5Z9lKhzF6QCYeB5-tTm4AcIl4FCQBy8E"
 #     DEBUG = True
 #     load_dotenv()
 #     GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
-
-
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
 GOOGLE_JS_KEY = "AIzaSyDcpvFtRDSx4pt3Fuf8etylEYKyUfm9knM" # this key has a Website restrictions
