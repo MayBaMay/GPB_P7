b53652b0923433f636ebddfb854b781fc5e550bf
MayMay <maylis.baschet@gmail.com>
2020-01-06 11:31:08 +0100

    test var .env & var heroku

--- config.py
+++ config.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+    GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_KEY")
+except ImportError:
+    GOOGLE_GEO_KEY = os.environ.get('GOOGLE_GEO_KEY')
+
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
 GOOGLE_JS_KEY = "AIzaSyDcpvFtRDSx4pt3Fuf8etylEYKyUfm9knM" # this key has a Website restrictions
-GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_KEY")
