a98903c1ce23686df0bd198b2baa463875d0a226
MayMay <maylis.baschet@gmail.com>
2020-01-06 10:28:09 +0100

    test with dotenv

--- config.py
+++ config.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
 GOOGLE_JS_KEY = "AIzaSyDcpvFtRDSx4pt3Fuf8etylEYKyUfm9knM" # this key has a Website restrictions
-GOOGLE_GEO_KEY = "YOUR_API_KEY"
+GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_KEY")
