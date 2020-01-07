6851e2ae8490f28fe4f27df55132ba2036fc9b31
MayMay <maylis.baschet@gmail.com>
2020-01-07 13:05:50 +0100

    suppr secretkey & tests_click

--- config.py
+++ config.py
@@ -7,5 +7,3 @@ from dotenv import load_dotenv
 load_dotenv() # load variables from file .env
 GOOGLE_GEO_KEY = os.getenv("GOOGLE_GEO_K")
 GOOGLE_JS_KEY = os.getenv("GOOGLE_JS_K")
-
-SECRET_KEY = os.environ.get('SECRET_KEY') or '@#qsf!?d/qd&:&039@#:,8'
